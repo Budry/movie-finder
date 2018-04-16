d5b4f69df23463f11c049423d1a25a677b3c1ef4
Budry <zarubaondra@gmail.com>
2018-04-16 11:01:44 +0200

    Added formats support

--- packages/terminal/src/main.go
+++ packages/terminal/src/main.go
@@ -3,34 +3,100 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"sync"
 
 	"github.com/budry/movie-finder/packages/terminal/src/fs"
 	"github.com/budry/movie-finder/packages/terminal/src/movies"
+	"github.com/urfave/cli"
+	"github.com/ryanuber/columnize"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "movie-finder"
+	app.HelpName = "movie-finder"
+	app.Commands = []cli.Command{
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "Print lists",
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "limit, l",
+					Usage: "Display limits",
+				},
+				cli.IntFlag{
+					Name:  "offset, o",
+					Usage: "Offset",
+				},
+				cli.StringFlag{
+					Name:  "format, f",
+					Value: "text",
+					Usage: "Display format",
+				},
+				cli.StringFlag{
+					Name:  "sortKey, k",
+					Value: "Name",
+					Usage: "Name of sort key",
+				},
+				cli.StringFlag{
+					Name:  "sortMethod, m",
+					Value: "DESC",
+					Usage: "Sort by",
+				},
+			},
+			Action: func(c *cli.Context) error {
 
-	if len(os.Args) != 2 {
-		panic("Invalid arguments")
+				if (c.NArg() != 1) {
+					panic("Missing path")
+				}
+
+				path := c.Args().Get(0)
+				limit := c.Int("limit")
+				offset := c.Int("offset")
+				sortKey := c.String("sortKey")
+				sortMethod := c.String("sortMethod")
+				format := c.String("format")
+
+				movieRecords := &movies.Movies{}
+				movieRecords.Init()
+
+				wg := &sync.WaitGroup{}
+
+				wg.Add(1)
+				fs.ProcessDirectory(path, wg, movieRecords)
+				wg.Wait()
+
+				if limit != 0 {
+					movieRecords.Limit(limit, offset)
+				}
+
+				movieRecords.Sort(sortKey, sortMethod)
+
+				moviesList := movieRecords.GetMovies()
+
+				if format == "text" {
+					outputs := make([]string, 0)
+					for _, movie := range moviesList {
+						outputs = append(outputs, fmt.Sprintf("%s|%s", movie.Name, movie.LastChange))
+					}
+					fmt.Println(columnize.SimpleFormat(outputs))
+					return nil
+				} else if format == "json" {
+					parsedMovies, err := json.Marshal(moviesList)
+					fmt.Println(string(parsedMovies))
+					return err
+				} else {
+					return nil
+				}
+			},
+		},
 	}
 
-	path := os.Args[1]
-
-	movieRecords := &movies.Movies{}
-	movieRecords.Init()
-
-	wg := &sync.WaitGroup{}
-
-	wg.Add(1)
-	fs.ProcessDirectory(path, wg, movieRecords)
-	wg.Wait()
-
-	parsedMovies, err := json.Marshal(movieRecords.GetMovies())
+	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
-	fmt.Println(string(parsedMovies))
 }
--- packages/terminal/src/movies/movies.go
+++ packages/terminal/src/movies/movies.go
@@ -1,6 +1,7 @@
 package movies
 
 import (
+	"sort"
 	"sync"
 	"time"
 )
@@ -15,6 +16,43 @@ type Movies struct {
 	Mutex *sync.Mutex
 }
 
+type MoviesSorter struct {
+	Movies *Movies
+	Key    string
+	Method string
+}
+
+func (moviesSorter *MoviesSorter) Len() int {
+	return len(moviesSorter.Movies.Data)
+}
+
+func (moviesSorter *MoviesSorter) Swap(i, j int) {
+	moviesSorter.Movies.Data[i], moviesSorter.Movies.Data[j] = moviesSorter.Movies.Data[j], moviesSorter.Movies.Data[i]
+}
+
+func (moviesSorter *MoviesSorter) Less(i, j int) bool {
+	movie1 := moviesSorter.Movies.Data[i]
+	movie2 := moviesSorter.Movies.Data[j]
+
+	compare := func (value1, value2 string) bool {
+		if moviesSorter.Method == "ASC" {
+			return len(value1) < len(value2)
+		} else if moviesSorter.Method == "DESC" {
+			return len(value1) > len(value2)
+		} else {
+			panic("Invalid sort method")
+		}
+	}
+
+	if moviesSorter.Key == "Name" {
+		return compare(movie1.Name, movie2.Name)
+	} else if moviesSorter.Key == "LastChange" {
+		return compare(movie1.LastChange.String(), movie2.LastChange.String())
+	} else {
+		panic("Invalid sort key")
+	}
+}
+
 func (movies *Movies) Init() {
 	movies.Data = make([]*Movie, 0)
 	movies.Mutex = &sync.Mutex{}
@@ -25,6 +63,14 @@ func (movies *Movies) InsertMovie(name string, lastChange time.Time) {
 	movie := &Movie{name, lastChange}
 	movies.Data = append(movies.Data, movie)
 	movies.Mutex.Unlock()
+}
+
+func (movies *Movies) Limit(limit int, offset int) {
+	movies.Data = movies.Data[offset : offset+limit]
+}
+
+func (movies *Movies) Sort(key string, method string) {
+	sort.Sort(&MoviesSorter{movies, key, method})
 }
 
 func (movies Movies) GetMovies() []*Movie {

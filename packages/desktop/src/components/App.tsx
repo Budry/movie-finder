8eac0d37c7a76cba9355de5b600d7922152d7c20
Budry <zarubaondra@gmail.com>
2018-04-16 21:43:09 +0200

    Removed mess

--- packages/desktop/src/components/App.tsx
+++ packages/desktop/src/components/App.tsx
@@ -8,49 +8,13 @@
  */
 
 import * as React from 'react'
-import {execFile} from "child_process"
-const {dialog} = require('electron').remote;
 
-interface State {
-    output: string
-}
-
-class App extends React.Component<{}, State> {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            output: ""
-        }
-    }
-
-    handleClick = (e) => {
-        e.preventDefault();
-        const directory = dialog.showOpenDialog({
-            properties: ['openDirectory']
-        });
-        if (directory) {
-            execFile(__dirname + "/../../external/movie-finder", [
-                "list",
-                "--format", "json",
-                "--limit", "10",
-                directory[0]
-            ], {maxBuffer: 10000 * 1024}, (err, stdout, stderr) => {
-                if (err) {
-                    this.setState({output: stderr})
-                } else {
-                    this.setState({output: stdout})
-                }
-            });
-        }
-    };
+class App extends React.Component<{}, {}> {
 
     render() {
         return (
             <div>
                 <h1>Hello world</h1>
-                <a href={""} onClick={this.handleClick}>Run</a>
-                {this.state.output}
             </div>
         );
     }
